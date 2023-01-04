8f40e9d7b92175063ef7c7d41888bc4cbc24027f
Andreas Roth <andreas.roth22@gmail.com>
2023-01-04 21:56:57 +0100

    Add 2022/day6 - part 2 solution

--- 2022/day6/src/main/java/tuning/App.java
+++ 2022/day6/src/main/java/tuning/App.java
@@ -4,7 +4,19 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println("\n--- Advent of Code 2022 - Day 6: Tuning Trouble ---\n");
-    int firstMarker = SignalProcessor.firstMarkerAfter(args[0], 4);
+    if (args == null || args.length != 2 || !isInteger(args[0])) {
+      System.out.println("Wrong usage! First provide the markerLength, then provide the signal...");
+    }
+    int firstMarker = SignalProcessor.firstMarkerAfter(args[1], Integer.valueOf(args[0]));
     System.out.println(String.format("First marker after character: %s", firstMarker));
+  }
+
+  private static boolean isInteger(String arg) {
+    try {
+      Integer.valueOf(arg);
+    } catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
   }
 }
